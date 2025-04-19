b83031af51859ddb90d4f3063a4efb8d90b77e06
Smile <piyushmishra1725@gmail.com>
2025-04-19 12:29:10 +0530

    End of section-4

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(DataContext context,ITokenService tokenService):BaseApiController
+{
+    [HttpPost("register")] //account/register
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+    {
+        if(await UserExists(registerDto.Username))
+        {
+            return BadRequest("UserName is taken. Please try a new one");
+        }
+        using var hmac=new HMACSHA512();  //code to encript the password,this is callomg the hashalgorithm we are not injecting this class into our controller we are just using it in our method
+
+        var user=new AppUser
+        {
+            UserName = registerDto.Username.ToLower(),
+            PasswordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt=hmac.Key
+        };
+        
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return new UserDto
+        {
+            Username=user.UserName,
+            Token=tokenService.CreateToken(user)
+        };
+    } 
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>>Login(LoginDto loginDto)
+    {
+        var user=await context.Users.FirstOrDefaultAsync(x=>x.UserName.ToLower()==loginDto.Username.ToLower());
+        if(user==null) return Unauthorized("Invalid Username");
+
+        using var hmac= new HMACSHA512(user.PasswordSalt);
+        var computedHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for(int i=0;i<computedHash.Length;i++)
+        {
+            if(computedHash[i]!=user.PasswordHash[i]) return Unauthorized("Invalid password");
+        }
+        return new UserDto
+        {
+            Username=user.UserName,
+            Token=tokenService.CreateToken(user)
+        };
+
+    }
+    private async Task<bool> UserExists(string username) //This is added so that every username will be unique and won't be repeated
+    {
+        return await context.Users.AnyAsync(x => x.UserName.ToLower()==username.ToLower());
+    }
+
+}
